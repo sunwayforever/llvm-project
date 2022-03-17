f027fbf93f6fbb7326bd959b5f02a5040d677a58
sunway <wei.sun@hexintek.com>
2022-03-17 11:39:32 +0800

    0063-RISCV-MC-layer-support-for-the-remaining-RVC-instruc.patch

--- llvm/test/MC/RISCV/rv64c-valid.s
+++ llvm/test/MC/RISCV/rv64c-valid.s
@@ -28,3 +28,31 @@ c.ld    a4, 0(a3)
 # CHECK: encoding: [0xfc,0xfe]
 # CHECK-NO-EXT:  error: instruction use requires an option to be enabled
 c.sd    a5, 248(a3)
+
+# CHECK-INST: c.subw   a3, a4
+# CHECK: encoding: [0x99,0x9e]
+c.subw   a3, a4
+# CHECK-INST: c.addw   a0, a2
+# CHECK: encoding: [0x31,0x9d]
+# CHECK-NO-EXT:  error: instruction use requires an option to be enabled
+c.addw   a0, a2
+
+# CHECK-INST: c.addiw  a3, -32
+# CHECK: encoding: [0x81,0x36]
+# CHECK-NO-EXT:  error: instruction use requires an option to be enabled
+c.addiw  a3, -32
+# CHECK-INST: c.addiw  a3, 31
+# CHECK: encoding: [0xfd,0x26]
+# CHECK-NO-EXT:  error: instruction use requires an option to be enabled
+c.addiw  a3, 31
+
+# CHECK-INST: c.slli  s0, 1
+# CHECK: encoding: [0x06,0x04]
+# CHECK-NO-EXT:  error: instruction use requires an option to be enabled
+c.slli  s0, 1
+# CHECK-INST: c.srli  a3, 63
+# CHECK: encoding: [0xfd,0x92]
+c.srli  a3, 63
+# CHECK-INST: c.srai  a2, 63
+# CHECK: encoding: [0x7d,0x96]
+c.srai  a2, 63
