30855d98924cb40b908fe45398e2776105ee80e5
sunway <wei.sun@hexintek.com>
2023-05-10 19:19:01 +0800

    nil

--- rebuild_cpu0.sh
+++ rebuild_cpu0.sh
@@ -2,5 +2,5 @@
 rm -rf build && mkdir build
 pushd build
 cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang   -DLLVM_TARGETS_TO_BUILD=Cpu0 -DLLVM_ENABLE_PROJECTS="clang" -DLLVM_ENABLE_LLD=True -DLLVM_OPTIMIZED_TABLEGEN=On -DLLVM_PARALLEL_LINK_JOBS=1 -G "Ninja" ../llvm
-ninja
+ninja llc
 popd
