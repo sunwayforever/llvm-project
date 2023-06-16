6cd45dfd61af319bc0a266ed3d73bdd6e8e39852
sunway <sunwayforever@gmail.com>
2023-06-16 12:13:26 +0800

    add linpack.c

--- llvm/lib/Target/Toy/ToyInstrInfo.cpp
+++ llvm/lib/Target/Toy/ToyInstrInfo.cpp
@@ -4,6 +4,9 @@
 #define GET_INSTRINFO_CTOR_DTOR
 #include "ToyGenInstrInfo.inc"
 
+#define GET_REGINFO_HEADER
+#include "ToyGenRegisterInfo.inc"
+
 using namespace llvm;
 
 ToyInstrInfo::ToyInstrInfo() : ToyGenInstrInfo() {}
@@ -15,7 +18,18 @@ void ToyInstrInfo::storeRegToStackSlot(MachineBasicBlock &MBB,
                                        const TargetRegisterInfo *TRI,
                                        Register VReg) const {
   DebugLoc DL;
-  BuildMI(MBB, MI, DL, get(Toy::STOREWFI))
+  int Opcode = 0;
+  if (Toy::GPRRegClass.hasSubClassEq(RC))
+    Opcode = Toy::STOREWFI;
+  else if (Toy::FPRRegClass.hasSubClassEq(RC))
+    Opcode = Toy::STORFFI;
+  else if (Toy::FPR64RegClass.hasSubClassEq(RC))
+    Opcode = Toy::STORDFI;
+  else {
+    llvm_unreachable("Impossible store reg to stack");
+  }
+
+  BuildMI(MBB, MI, DL, get(Opcode))
       .addReg(SrcReg, getKillRegState(isKill))
       .addFrameIndex(FI)
       .addImm(0);
@@ -28,7 +42,18 @@ void ToyInstrInfo::loadRegFromStackSlot(MachineBasicBlock &MBB,
                                         const TargetRegisterInfo *TRI,
                                         Register VReg) const {
   DebugLoc DL;
-  BuildMI(MBB, MI, DL, get(Toy::LOADWFI), DestReg).addFrameIndex(FI).addImm(0);
+  int Opcode = 0;
+  if (Toy::GPRRegClass.hasSubClassEq(RC))
+    Opcode = Toy::LOADWFI;
+  else if (Toy::FPRRegClass.hasSubClassEq(RC))
+    Opcode = Toy::LOADFFI;
+  else if (Toy::FPR64RegClass.hasSubClassEq(RC))
+    Opcode = Toy::LOADDFI;
+  else {
+    llvm_unreachable("Impossible load reg from stack");
+  }
+
+  BuildMI(MBB, MI, DL, get(Opcode), DestReg).addFrameIndex(FI).addImm(0);
 }
 
 void ToyInstrInfo::copyPhysReg(MachineBasicBlock &MBB,
@@ -36,9 +61,16 @@ void ToyInstrInfo::copyPhysReg(MachineBasicBlock &MBB,
                                const DebugLoc &DL, MCRegister DestReg,
                                MCRegister SrcReg, bool KillSrc) const {
 
-  MachineInstrBuilder MIB = BuildMI(MBB, I, DL, get(Toy::ADD));
+  int Opcode = 0;
+  if (Toy::GPRRegClass.contains(SrcReg)) {
+    Opcode = Toy::MV;
+  } else if (Toy::FPR64RegClass.contains(SrcReg)) {
+    // NOTE: both FPR and FPR64 contains f-regs
+    Opcode = Toy::FMVD;
+  } else {
+    llvm_unreachable("Impossible reg-to-reg copy");
+  }
 
-  MIB.addReg(DestReg, RegState::Define);
-  MIB.addReg(Toy::ZERO);
+  MachineInstrBuilder MIB = BuildMI(MBB, I, DL, get(Opcode), DestReg);
   MIB.addReg(SrcReg, getKillRegState(KillSrc));
 }
