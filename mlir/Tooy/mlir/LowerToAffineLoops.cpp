45bc4588416e9339a34ad090b25354d6ac79c05c
sunway <sunwayforever@gmail.com>
2024-02-26 16:22:47 +0800

    add div

--- mlir/Tooy/mlir/LowerToAffineLoops.cpp
+++ mlir/Tooy/mlir/LowerToAffineLoops.cpp
@@ -129,6 +129,7 @@ struct BinaryOpLowering : public ConversionPattern {
 };
 using AddOpLowering = BinaryOpLowering<toy::AddOp, arith::AddFOp>;
 using MulOpLowering = BinaryOpLowering<toy::MulOp, arith::MulFOp>;
+using DivOpLowering = BinaryOpLowering<toy::DivOp, arith::DivFOp>;
 
 //===----------------------------------------------------------------------===//
 // ToyToAffine RewritePatterns: Constant operations
@@ -348,7 +349,7 @@ void ToyToAffineLoweringPass::runOnOperation() {
   // Now that the conversion target has been defined, we just need to provide
   // the set of patterns that will lower the Toy operations.
   RewritePatternSet patterns(&getContext());
-  patterns.add<AddOpLowering, ConstantOpLowering, FuncOpLowering, MulOpLowering,
+  patterns.add<AddOpLowering, ConstantOpLowering, FuncOpLowering, MulOpLowering, DivOpLowering,
                PrintOpLowering, ReturnOpLowering, TransposeOpLowering>(
       &getContext());
 
